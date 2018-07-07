99629eb303d25a79e830705625abbcd49fad0b65
YakovDeLeon <swimmerfromrussia@gmail.com>
2018-07-07 12:53:50 +0300

    Задача 2

---
+++ task2.cpp
@@ -0,0 +1,26 @@
+#include <cmath>
+bool checkPrime(unsigned int value)
+{
+	for (size_t i = 2; i <= sqrt(value); i++)
+	{
+		if (value % i == 0)
+			return false;
+	}
+	return true;
+}
+unsigned int nPrime(unsigned n)
+{
+	int tmp = 0;
+	unsigned int i = 2;
+	while (tmp != n)
+	{
+		if (checkPrime(i++))
+			tmp++;
+	}
+	return i-1;
+}
+unsigned int nextPrime(unsigned int value) 
+{
+	while (!checkPrime(++value));
+	return value;
+}
---
+++ task2.h
@@ -0,0 +1,4 @@
+#pragma once
+bool checkPrime(unsigned int value);
+unsigned int nPrime(unsigned n);
+unsigned int nextPrime(unsigned int value);
